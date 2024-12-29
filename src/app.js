c66f99a7e7cfe18d2529c2a084f2dcb21cb67c86
harish-kush <h.kush2005@gmail.com>
2024-12-29 19:24:03 +0530

    routers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ORIGIN_URL,
+    origin: process.env.ORIGIN_URL,
     credentials: true,
 }));
 
@@ -14,7 +17,11 @@ app.use(express.urlencoded({ extended: true , limit: '50kb'}));
 
 app.use(express.static('public'));
 
-
 app.use(cookieParser());
+
+// router
+import { router } from "./routes/user.routes.js";
+app.use("/api/v1/users", router);
+// http://localhost:8000/api/v1/users/register
 
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,3 @@
-// yaha jo bhi apn likhenge wo har jagah use hoga
-
-const asyncHandler = (handleRequest) => {
-    (req,res,next)=>{
-        Promise.resolve(handleRequest(req,res,next)).catch((err)=>{
-            next(err);
-        })
-    }
-} 
-
-/*
-const asyncHandler = (fn) = async(req,res,next)=>{
-    try{
-    await fn(req,res,next);
-    }catch(err){ 
-      res.status(500).json({message:err.message});
-    }
-
-*/
-
-export {asyncHandler};
+export const asyncHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
